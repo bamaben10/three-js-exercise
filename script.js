bfadd5e1a1c5500b1c41fb735a3dd01f1679b239
Benjamin Salmon <bensalmon8@gmail.com>
2022-09-11 18:20:56 -0400

    fix error

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// Scene
-const scene = new THREE.scene();
-
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
 // Sizes
 const sizes = {
@@ -13,17 +7,25 @@ const sizes = {
   height: 600,
 };
 
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "#ff0000",
+});
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
 // Renderer
-const canvas = document.querySelector(".webgl");
-console.log(canvas);
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-
 renderer.render(scene, camera);
